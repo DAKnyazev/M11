d1269a06ec4b7a79002e0489976590b3d6a588c5
DAKnyazev <kn56@yandex.ru>
2018-09-21 20:30:33 +0300

    Редизайн основной страницы

--- M11/M11/MainPage.xaml.cs
+++ M11/M11/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace M11
@@ -23,51 +25,49 @@ namespace M11
 	            await Navigation.PushAsync(new AuthPage());
                 return;
             }
-            
-	        BalanceLabel.FormattedText.Spans[0].Text = App.Info.Balance;
-            BalanceLabel.FormattedText.Spans[1].Text = " Р";
-            TicketLayout.Children.Clear();
-            foreach (var ticket in App.Info.Tickets)
-            {
-                TicketLayout.Children.Add(new StackLayout
-                {
-                    Children =
-                    {
-                        new Label
-                        {
-                            Text = ticket.ContractNumber
-                        },
-                        new Label
-                        {
-                            Text = ticket.TransponderNumber
-                        },
-                        new Label
-                        {
-                            Text = ticket.Description
-                        },
-                        new Label
-                        {
-                            Text = ticket.StartDate.ToString("dd.MM.yyyy HH:mm")
-                        },
-                        new Label
-                        {
-                            Text = ticket.ExpiryDate.ToString("dd.MM.yyyy HH:mm")
-                        },
-                        new Label
-                        {
-                            Text = ticket.RemainingTripsCount.ToString()
-                        },
-                        new Label
-                        {
-                            Text = ticket.Status
-                        },
-                        new Label
-                        {
-                            Text = ticket.IsFairPriceOptionIncluded ? "Честная цена" : string.Empty
-                        }
-                    }
-                });
+
+	        BalanceLabel.Text = App.Info.Balance;
+	        TicketLayout.Children.Clear();
+	        if (App.Info.Tickets.Any())
+	        {
+	            TicketLayout.Children.Add(new Label { Text = "Абонементы:", FontSize = 36 });
             }
-        }
+	        foreach (var ticket in App.Info.Tickets)
+	        {
+	            var layout = new RelativeLayout();
+	            var ticketDescriptions = ticket.Description.Split(',');
+                layout.Children.Add(new Label
+                    {
+                        Text = ticketDescriptions.Length > 2 ? ticketDescriptions[2] : string.Empty,
+                        FontSize = 24
+                    },
+                    Constraint.RelativeToParent(parent => 0),
+                    null, 
+                    Constraint.RelativeToParent(parent => parent.Width),
+                    Constraint.Constant(36));
+	            var count = ticketDescriptions.Length > 0 ? Regex.Match(ticketDescriptions[0], @"\d+").Value : string.Empty;
+	            var remainingCountText = string.IsNullOrWhiteSpace(count)
+	                ? $"осталось поездок: {ticket.RemainingTripsCount}"
+	                : $"осталось поездок: {ticket.RemainingTripsCount} (из {count})";
+
+                layout.Children.Add(new Label { Text = $"{ticket.Status}, {remainingCountText}" },
+	                Constraint.RelativeToParent(parent => 0),
+	                Constraint.Constant(30),
+	                Constraint.RelativeToParent(parent => parent.Width),
+	                Constraint.Constant(20));
+                layout.Children.Add(new Label { Text = $"Использовать до: {ticket.ExpiryDate:dd.MM.yyyy HH:mm}" },
+                    Constraint.RelativeToParent(parent => 0),
+                    Constraint.Constant(50),
+                    Constraint.RelativeToParent(parent => parent.Width),
+                    Constraint.Constant(20));
+                layout.Children.Add(new Label { BackgroundColor = Color.Black },
+                    Constraint.RelativeToParent(parent => 0),
+                    Constraint.Constant(70),
+                    Constraint.RelativeToParent(parent => parent.Width),
+                    Constraint.Constant(1));
+                
+                TicketLayout.Children.Add(layout);
+	        }
+	    }
     }
 }
