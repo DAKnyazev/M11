9e26011ce3f4abf952e3b3caee2da99013906943
DAKnyazev <kn56@yandex.ru>
2018-09-21 20:31:23 +0300

    Начало редизайна страницы статистики

---
+++ M11.Common/Extentions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace M11.Common.Extentions
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input)
+        {
+            switch (input)
+            {
+                case null: return string.Empty;
+                case "": return string.Empty;
+                default: return input.First().ToString().ToUpper() + input.Substring(1);
+            }
+        }
+    }
+}
--- M11/M11/StatisticPage.xaml.cs
+++ M11/M11/StatisticPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using M11.Common.Enums;
+using M11.Common.Extentions;
+using M11.Services;
+using Xamarin.Forms;
 
 namespace M11
 {
@@ -8,5 +14,37 @@ namespace M11
 		{
 			InitializeComponent();
 		}
-	}
+
+        protected override async void OnAppearing()
+        {
+            await InitializeAsync();
+        }
+
+        private async Task InitializeAsync()
+        {
+            if (!await App.TryGetInfo())
+            {
+                await Navigation.PushAsync(new AuthPage());
+                return;
+            }
+
+            StatisticLayout.Children.Clear();
+            var info = await new InfoService().GetAccountInfo(
+                App.Info.Links.FirstOrDefault(x => x.Type == LinkType.Account)?.RelativeUrl,
+                App.Info.CookieContainer,
+                DateTime.Now,
+                DateTime.Now.AddMonths(-5));
+            foreach (var item in info.BillSummaryList)
+            {
+                var layout = new RelativeLayout();
+                layout.Children.Add(new Label { Text = item.Period.ToString("MMMM yyyy").FirstCharToUpper() },
+                    Constraint.RelativeToParent(parent => 0),
+                    null,
+                    Constraint.RelativeToParent(parent => parent.Width),
+                    Constraint.Constant(36));
+
+                StatisticLayout.Children.Add(layout);
+            }
+        }
+    }
 }
